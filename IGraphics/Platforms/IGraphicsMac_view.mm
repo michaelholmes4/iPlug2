90fe6889748b39bbbfbfa92d3845b716405cbabe
Michael Holmes <michaelholmes@hcloudh.com>
2025-04-11 22:05:48 +1000

    Fix grabbing characters

--- IGraphics/Platforms/IGraphicsMac_view.mm
+++ IGraphics/Platforms/IGraphicsMac_view.mm
@@ -95,7 +95,7 @@ static int MacKeyEventToVK(NSEvent* pEvent, int& flag)
   {
     NSString *str = NULL;
     
-    if (!str || ![str length]) str = [pEvent characters];
+    if (!str || ![str length]) str = [pEvent charactersIgnoringModifiers];
     
     if (!str || ![str length])
     {
